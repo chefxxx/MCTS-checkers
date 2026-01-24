ea106ed91ddc124d219b331ffb60e9a500da9d57
chefxxx <mateusz.mm100@gmail.com>
2026-01-24 13:05:48 +0100

    Add white pawn single attacks tests

--- tests/game_test_white.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by chefxx on 18.01.2026.
-//
-
-#include <gtest/gtest.h>
-
-#include "game_engine.h"
-
-TEST(GameTest, whiteTest) { ASSERT_TRUE(true); }
--- tests/gpu_tests.cu
+++ tests/gpu_tests.cu
@@ -13,15 +13,6 @@
 constexpr int TEST_BLOCKS = 1;
 constexpr int TEST_THREADS = 1;
 
-// TEST_F(GpuRolloutTests, firstTest)
-// {
-//     Board board;
-//     board.initStartingBoard();
-//     const MctsNode node{nullptr, board, white};
-//     prepare_gpu_const_mem();
-//     const auto result = rollout_gpu(&node);
-// }
-
 class GpuMovegenTests : public ::testing::Test
 {
     void SetUp() override
@@ -130,4 +121,101 @@ TEST_F(GpuMovegenTests, blackPawnMoveDownwardsRight) {
 
     ASSERT_EQ(1ULL << 45, h_result.from_mask);
     ASSERT_EQ(1ULL << 38, h_result.to_mask);
+}
+
+
+TEST_F(GpuMovegenTests, whitePawnCaptureUpRight) {
+    GPU_Board board;
+    board.pawns[white] = 1ULL << 18;
+    board.pawns[black] = 1ULL << 27;
+
+    const auto d_boardPtr = mem_cuda::allocateAndCopyGPU_FromHostObject(board);
+    const auto d_movePtr = mem_cuda::make_unique<GPU_Move>();
+    const auto d_statesPtr = mem_cuda::make_unique<curandState>(1);
+    setup_curand_kernel<<<1, 1>>>(d_statesPtr.get(), 555);
+    CUDA_CHECK_KERNEL();
+    CUDA_SYNC_CHECK();
+
+    test_kernel<<<1, 1>>>(d_statesPtr.get(), d_boardPtr.get(), white, d_movePtr.get());
+    CUDA_CHECK_KERNEL();
+    CUDA_SYNC_CHECK();
+
+    GPU_Move h_result;
+    checkCudaErrors(cudaMemcpy(&h_result, d_movePtr.get(), sizeof(GPU_Move), cudaMemcpyDeviceToHost));
+
+    ASSERT_EQ(1ULL << 18, h_result.from_mask);
+    ASSERT_EQ(1ULL << 36, h_result.to_mask);
+    ASSERT_EQ(1ULL << 27, h_result.captures_mask);
+}
+
+TEST_F(GpuMovegenTests, whitePawnCaptureUpLeft) {
+    GPU_Board board;
+    board.pawns[white] = 1ULL << 18;
+    board.pawns[black] = 1ULL << 25;
+
+    const auto d_boardPtr = mem_cuda::allocateAndCopyGPU_FromHostObject(board);
+    const auto d_movePtr = mem_cuda::make_unique<GPU_Move>();
+    const auto d_statesPtr = mem_cuda::make_unique<curandState>(1);
+    setup_curand_kernel<<<1, 1>>>(d_statesPtr.get(), 555);
+    CUDA_CHECK_KERNEL();
+    CUDA_SYNC_CHECK();
+
+    test_kernel<<<1, 1>>>(d_statesPtr.get(), d_boardPtr.get(), white, d_movePtr.get());
+    CUDA_CHECK_KERNEL();
+    CUDA_SYNC_CHECK();
+
+    GPU_Move h_result;
+    checkCudaErrors(cudaMemcpy(&h_result, d_movePtr.get(), sizeof(GPU_Move), cudaMemcpyDeviceToHost));
+
+    ASSERT_EQ(1ULL << 18, h_result.from_mask);
+    ASSERT_EQ(1ULL << 32, h_result.to_mask);
+    ASSERT_EQ(1ULL << 25, h_result.captures_mask);
+}
+
+TEST_F(GpuMovegenTests, whitePawnCaptureDownLeft) {
+    GPU_Board board;
+    board.pawns[white] = 1ULL << 18;
+    board.pawns[black] = 1ULL << 9;
+
+    const auto d_boardPtr = mem_cuda::allocateAndCopyGPU_FromHostObject(board);
+    const auto d_movePtr = mem_cuda::make_unique<GPU_Move>();
+    const auto d_statesPtr = mem_cuda::make_unique<curandState>(1);
+    setup_curand_kernel<<<1, 1>>>(d_statesPtr.get(), 555);
+    CUDA_CHECK_KERNEL();
+    CUDA_SYNC_CHECK();
+
+    test_kernel<<<1, 1>>>(d_statesPtr.get(), d_boardPtr.get(), white, d_movePtr.get());
+    CUDA_CHECK_KERNEL();
+    CUDA_SYNC_CHECK();
+
+    GPU_Move h_result;
+    checkCudaErrors(cudaMemcpy(&h_result, d_movePtr.get(), sizeof(GPU_Move), cudaMemcpyDeviceToHost));
+
+    ASSERT_EQ(1ULL << 18, h_result.from_mask);
+    ASSERT_EQ(1ULL << 0, h_result.to_mask);
+    ASSERT_EQ(1ULL << 9, h_result.captures_mask);
+}
+
+TEST_F(GpuMovegenTests, whitePawnCaptureDownRight) {
+    GPU_Board board;
+    board.pawns[white] = 1ULL << 18;
+    board.pawns[black] = 1ULL << 11;
+
+    const auto d_boardPtr = mem_cuda::allocateAndCopyGPU_FromHostObject(board);
+    const auto d_movePtr = mem_cuda::make_unique<GPU_Move>();
+    const auto d_statesPtr = mem_cuda::make_unique<curandState>(1);
+    setup_curand_kernel<<<1, 1>>>(d_statesPtr.get(), 555);
+    CUDA_CHECK_KERNEL();
+    CUDA_SYNC_CHECK();
+
+    test_kernel<<<1, 1>>>(d_statesPtr.get(), d_boardPtr.get(), white, d_movePtr.get());
+    CUDA_CHECK_KERNEL();
+    CUDA_SYNC_CHECK();
+
+    GPU_Move h_result;
+    checkCudaErrors(cudaMemcpy(&h_result, d_movePtr.get(), sizeof(GPU_Move), cudaMemcpyDeviceToHost));
+
+    ASSERT_EQ(1ULL << 18, h_result.from_mask);
+    ASSERT_EQ(1ULL << 4, h_result.to_mask);
+    ASSERT_EQ(1ULL << 11, h_result.captures_mask);
 }
